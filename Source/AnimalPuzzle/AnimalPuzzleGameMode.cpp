7001580f8e3a68e0279077305cb44c82766d4f76
BunEvie04 <stugreen2005@gmail.com>
2025-02-24 14:36:28 +0000

    GameModeFix

--- Source/AnimalPuzzle/AnimalPuzzleGameMode.cpp
+++ Source/AnimalPuzzle/AnimalPuzzleGameMode.cpp
@@ -7,9 +7,9 @@
 AAnimalPuzzleGameMode::AAnimalPuzzleGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Dynamic/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
+		DefaultPawnClass = BP_ThirdPersonCharacter.Class;
 	}
 }
